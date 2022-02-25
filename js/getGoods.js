33627d7ff1fecc730b5aabe02dbcb48e18ccc589
romartiny <91370772+romartiny@users.noreply.github.com>
2022-02-25 11:57:41 +0300

    update
    3 step ended

--- js/getGoods.js
+++ js/getGoods.js
@@ -1,31 +1,78 @@
 const getGoods = () => {
     const links = document.querySelectorAll('.navigation-link');
+    const viewAll = document.querySelectorAll('.more');
 
-    const getData = () => {
+    renderGoods = (goods) => {
+        const goodsContainer = document.querySelector('.long-goods-list');
+
+        goodsContainer.innerHTML = '';
+
+        goods.forEach(good => {
+            const goodBlock = document.createElement('div');
+
+            goodBlock.classList.add('col-lg-3');
+            goodBlock.classList.add('col-sm-3');
+
+            goodBlock.innerHTML = `
+                     <div class="goods-card">
+						<span class="label ${good.label ? null : 'd-none'}">${good.label}</span>
+                        <img src="db/${good.img}" alt="${good.label}" class="goods-image">
+						<h3 class="goods-title">${good.name}</h3>
+						<p class="goods-description">${good.description}</p>
+						<button class="button goods-card-btn add-to-cart" data-id="${good.id}">
+							<span class="button-price">$${good.price}</span>
+						</button>
+					</div>
+            `;
+
+            goodsContainer.append(goodBlock);
+        })
+    }
+
+    const getData = (value, category) => {
         fetch('https://wildberries-training-project-default-rtdb.firebaseio.com/db.json') 
         .then((res) => res.json())
         .then((data) => {
-            localStorage.setItem('data', JSON.stringify(data));
-            console.log(data);
+            const array = category ? data.filter((item) => item[category] === value) : data;
+
+            localStorage.setItem('goods', JSON.stringify(array));
+
+            if (window.location.pathname !== "/goods.html") {
+                window.location.href = 'goods.html';
+            } else {
+                renderGoods(array);
+            }
         })
     }
 
     links.forEach((link) => {
-        link.addEventListener('click',(event) => {
-            event.preventDefault();
-            getData();
+        link.addEventListener('click', (event) => {
+            event.preventDefault(); 
+
+            const linkValue = link.textContent;
+            const category = link.dataset.field;
+
+            getData(linkValue, category);
         })
     })
 
-    // const data = localStorage.setItem('data', JSON.stringify(links));
+    viewAll.forEach((link) => {
+        link.addEventListener('click', (event) => {
+            console.log(viewAll);
+            event.preventDefault(); 
 
-    // const goods = JSON.parse(localStorage.getItem('data'));
+            const linkValue = link.textContent;
+            const category = link.dataset.field;
 
-    // // localStorage.removeItem('goods');
+            getData(linkValue, category);
+        })
+    })
 
-    // const getLoots = JSON.parse(localStorage.getItem('goods'));
+    if (localStorage.getItem('goods') && window.location.pathname === "/goods.html") {
+        renderGoods(JSON.parse(localStorage.getItem('goods')));
+    }
 
-    // console.log(data);
+
 }
 
 getGoods();
